1b48960ae954125fe25ce958367ae56a2fabc9dd
Looly <loolly@gmail.com>
2015-09-15 11:20:03 +0800

    Fix Action

--- src/main/java/com/xiaoleilu/loServer/action/Action.java
+++ src/main/java/com/xiaoleilu/loServer/action/Action.java
@@ -3,6 +3,12 @@ package com.xiaoleilu.loServer.action;
 import com.xiaoleilu.loServer.handler.Request;
 import com.xiaoleilu.loServer.handler.Response;
 
+/**
+ * 请求处理接口<br>
+ * 当用户请求某个Path，则调用相应Action的doAction方法
+ * @author Looly
+ *
+ */
 public interface Action {
 	public void doAction(Request request, Response response);
 }
--- src/main/java/com/xiaoleilu/loServer/annotation/Route.java
+++ src/main/java/com/xiaoleilu/loServer/annotation/Route.java
@@ -1,10 +1,8 @@
 package com.xiaoleilu.loServer.annotation;
 
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
 /**
  * 注解，用于自定义访问的URL路径<br>
@@ -12,7 +10,6 @@ import java.lang.annotation.Target;
  * @author loolly
  *
  */
-@Target({ElementType.TYPE})/*修饰的对象范围*/
 @Retention(RetentionPolicy.RUNTIME)/*保留的时间长短*/
 @Inherited/*只用于class，可被子类继承*/
 public @interface Route {
